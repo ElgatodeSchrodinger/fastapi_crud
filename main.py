c813353b06d9ab79bc3f8eedc756dafaac53abd2
Javier Quintana Taipe <javier.taipe.1998@gmail.com>
2021-07-26 12:27:06 -0500

    Adding CORS

--- main.py
+++ main.py
@@ -5,11 +5,25 @@ from fastapi import FastAPI
 from fastapi_crudrouter import SQLAlchemyCRUDRouter
 from router import employee_router, client_router, user_router, router, product_router
 from database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
+origins = [
+    "http://localhost:5000",
+    "http://0.0.0.0:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
